0edd1017904071890bcc449c904556594f9bcd90
Ben Young <developing.ben@gmail.com>
2024-03-13 15:51:17 -0500

    susi flow complete, except reset password

--- src/app/(modals)/susi.tsx
+++ src/app/(modals)/susi.tsx
@@ -30,6 +30,8 @@ enum AuthStrategy {
 }
 
 const Susi = () => {
+  useWarmUpBrowser();
+
   const {
     signIn,
     setActive: setActiveSI,
@@ -41,234 +43,24 @@ const Susi = () => {
     isLoaded: isSignUpLoaded,
   } = useSignUp();
 
+  const router = useRouter();
+
   const [emailAddress, setEmailAddress] = useState("");
   const [password, setPassword] = useState("");
   const [pendingVerification, setPendingVerification] = useState(false);
   const [code, setCode] = useState("");
   const [loading, setLoading] = useState(false);
+  const [intendToRegister, setIntendToRegister] = useState(false);
 
-  const onSignUpPress = async () => {
-    if (!isSignUpLoaded) {
-      return;
-    }
-    setLoading(true);
-
-    try {
-      // Create the user on Clerk
-      await signUp.create({
-        emailAddress,
-        password,
-      });
-      // Send verification Email
-      await signUp.prepareEmailAddressVerification({ strategy: "email_code" });
-
-      // change the UI to verify the email address
-      setPendingVerification(true);
-    } catch (err: any) {
-      alert(err.errors[0].message);
-    } finally {
-      setLoading(false);
-    }
+  const onRegisterPress = () => {
+    setIntendToRegister(true);
   };
 
-  const onVerify = async () => {
-    if (!isSignUpLoaded) {
-      return;
-    }
-    setLoading(true);
-
-    try {
-      const completeSignUp = await signUp.attemptEmailAddressVerification({
-        code,
-      });
-
-      await setActiveSU({ session: completeSignUp.createdSessionId });
-    } catch (err: any) {
-      alert(err.errors[0].message);
-    } finally {
-      setLoading(false);
-    }
-  };
-
-  const Register = () => {
-    // const { isLoaded, signUp, setActive } = useSignUp();
-
-    // const [emailAddress, setEmailAddress] = useState("");
-    // const [password, setPassword] = useState("");
-    // const [pendingVerification, setPendingVerification] = useState(false);
-    // const [code, setCode] = useState("");
-    // const [loading, setLoading] = useState(false);
-
-    // const onSignUpPress = async () => {
-    //   if (!isLoaded) {
-    //     return;
-    //   }
-    //   setLoading(true);
-
-    //   try {
-    //     // Create the user on Clerk
-    //     await signUp.create({
-    //       emailAddress,
-    //       password,
-    //     });
-    //     // Send verification Email
-    //     await signUp.prepareEmailAddressVerification({ strategy: "email_code" });
-
-    //     // change the UI to verify the email address
-    //     setPendingVerification(true);
-    //   } catch (err: any) {
-    //     alert(err.errors[0].message);
-    //   } finally {
-    //     setLoading(false);
-    //   }
-    // };
-
-    // const onVerify = async () => {
-    //   if (!isLoaded) {
-    //     return;
-    //   }
-    //   setLoading(true);
-
-    //   try {
-    //     const completeSignUp = await signUp.attemptEmailAddressVerification({
-    //       code,
-    //     });
-
-    //     await setActive({ session: completeSignUp.createdSessionId });
-    //   } catch (err: any) {
-    //     alert(err.errors[0].message);
-    //   } finally {
-    //     setLoading(false);
-    //   }
-    // };
-
-    return (
-      <View style={styles.container}>
-        <Stack.Screen options={{ headerBackVisible: !pendingVerification }} />
-        <MonoText style={styles.title}>taste map</MonoText>
-        <ActivityIndicator
-          animating={loading}
-          size="large"
-          color={COLORS.honey}
-        />
-        {!pendingVerification && (
-          <>
-            <TextInput
-              autoCapitalize="none"
-              placeholder="Email"
-              value={emailAddress}
-              onChangeText={setEmailAddress}
-              style={[defaultStyles.inputField, { marginBottom: 20 }]}
-            />
-            <TextInput
-              placeholder="Password"
-              value={password}
-              onChangeText={setPassword}
-              secureTextEntry
-              style={[defaultStyles.inputField, { marginBottom: 20 }]}
-            />
-
-            <Button
-              onPress={onSignUpPress}
-              title="Sign up"
-              color={"#6c47ff"}
-            ></Button>
-            <Button
-              onPress={() => router.back()}
-              title="Back to Login"
-              color={"#6c47ff"}
-            ></Button>
-          </>
-        )}
-
-        {pendingVerification && (
-          <>
-            <View>
-              <TextInput
-                value={code}
-                placeholder="Code..."
-                style={defaultStyles.inputField}
-                onChangeText={setCode}
-              />
-            </View>
-            <Button
-              onPress={onVerify}
-              title="Verify Email"
-              color={COLORS.clean}
-            ></Button>
-          </>
-        )}
-
-        {setActiveSU && (
-          <Button
-            onPress={() => router.back()}
-            title="Back to Login"
-            color={"#6c47ff"}
-          ></Button>
-        )}
-      </View>
-    );
-  };
+  const { startOAuthFlow: googleAuth } = useOAuth({
+    strategy: "oauth_google",
+  });
 
   const Login = () => {
-    useWarmUpBrowser();
-    const router = useRouter();
-
-    const { signIn, setActive, isLoaded } = useSignIn();
-    const [intendToRegister, setIntendToRegister] = useState(false);
-
-    const [emailAddress, setEmailAddress] = useState("");
-    const [password, setPassword] = useState("");
-    const [loading, setLoading] = useState(false);
-
-    const { startOAuthFlow: googleAuth } = useOAuth({
-      strategy: "oauth_google",
-    });
-
-    const onSelectAuth = async (strategy: AuthStrategy) => {
-      const selectAuth = {
-        [AuthStrategy.Google]: googleAuth,
-      }[strategy];
-
-      try {
-        const { createdSessionId, setActive } = await selectAuth();
-
-        if (createdSessionId) {
-          setActive!({ session: createdSessionId });
-          router.back();
-        }
-      } catch (err) {
-        console.error("OAuth error", err);
-      }
-    };
-
-    const onRegisterPress = () => {
-      setIntendToRegister(true);
-    };
-
-    if (intendToRegister) {
-      return <Register />;
-    }
-
-    const onSignInPress = async () => {
-      if (!isLoaded) {
-        return;
-      }
-      setLoading(true);
-      try {
-        const completeSignIn = await signIn.create({
-          identifier: emailAddress,
-          password,
-        });
-
-        await setActive({ session: completeSignIn.createdSessionId });
-      } catch (error: any) {
-        alert(error.error[0].message);
-      } finally {
-        setLoading(false);
-      }
-    };
-
     return (
       <View style={styles.container}>
         <MonoText style={styles.title}>taste map</MonoText>
@@ -333,23 +125,172 @@ const Susi = () => {
     );
   };
 
-  const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      padding: 20,
-    },
-    title: {
-      fontSize: 36,
-      fontWeight: "bold",
-      color: COLORS.cerulean,
-      textAlign: "center",
-      marginBottom: 20,
-    },
-    links: {
-      textAlign: "center",
-      paddingBottom: 2,
-    },
-  });
+  const Register = () => {
+    return (
+      <View style={styles.container}>
+        <Stack.Screen options={{ headerBackVisible: !pendingVerification }} />
+        <MonoText style={styles.title}>taste map</MonoText>
+        <ActivityIndicator
+          animating={loading}
+          size="large"
+          color={COLORS.honey}
+        />
+        {!pendingVerification && (
+          <>
+            <TextInput
+              autoCapitalize="none"
+              placeholder="Email"
+              value={emailAddress}
+              onChangeText={setEmailAddress}
+              style={[defaultStyles.inputField, { marginBottom: 20 }]}
+            />
+            <TextInput
+              placeholder="Password"
+              value={password}
+              onChangeText={setPassword}
+              secureTextEntry
+              style={[defaultStyles.inputField, { marginBottom: 20 }]}
+            />
+            <View>
+              <Pressable
+                style={[defaultStyles.button, { marginBottom: 10 }]}
+                onPress={onSignUpPress}
+              >
+                <Text style={defaultStyles.buttonText}>Sign Up</Text>
+              </Pressable>
+              <Pressable
+                style={defaultStyles.buttonOutline}
+                onPress={() => router.back()}
+              >
+                <Text style={defaultStyles.buttonOutlineText}>Back to It</Text>
+              </Pressable>
+            </View>
+          </>
+        )}
+
+        {pendingVerification && (
+          <>
+            <View>
+              <TextInput
+                value={code}
+                placeholder="Enter the code from your email"
+                style={[defaultStyles.inputField, { marginBottom: 20 }]}
+                onChangeText={setCode}
+              />
+            </View>
+            <Pressable style={defaultStyles.buttonOutline} onPress={onVerify}>
+              <Text style={defaultStyles.buttonOutlineText}>Verify Email</Text>
+            </Pressable>
+          </>
+        )}
+      </View>
+    );
+  };
+
+  const onSelectAuth = async (strategy: AuthStrategy) => {
+    const selectAuth = {
+      [AuthStrategy.Google]: googleAuth,
+    }[strategy];
+
+    try {
+      const { createdSessionId, setActive } = await selectAuth();
+
+      if (createdSessionId) {
+        setActive!({ session: createdSessionId });
+        router.back();
+      }
+    } catch (err) {
+      console.error("OAuth error", err);
+    }
+  };
+
+  const onSignUpPress = async () => {
+    if (!isSignUpLoaded) {
+      return;
+    }
+    setLoading(true);
+
+    try {
+      // Create the user on Clerk
+      await signUp.create({
+        emailAddress,
+        password,
+      });
+      // Send verification Email
+      await signUp.prepareEmailAddressVerification({ strategy: "email_code" });
+
+      // change the UI to verify the email address
+      setPendingVerification(true);
+    } catch (err: any) {
+      alert(err.errors[0].message);
+    } finally {
+      await setActiveSU({ session: signUp.createdSessionId });
+      setLoading(false);
+    }
+  };
+
+  const onVerify = async () => {
+    if (!isSignUpLoaded) {
+      return;
+    }
+    setLoading(true);
+
+    try {
+      const completeSignUp = await signUp.attemptEmailAddressVerification({
+        code,
+      });
+
+      await setActiveSU({ session: completeSignUp.createdSessionId });
+    } catch (err: any) {
+      alert(err.errors[0].message);
+    } finally {
+      setLoading(false);
+      router.back();
+    }
+  };
+
+  const onSignInPress = async () => {
+    if (!isSignInLoaded) {
+      return;
+    }
+    setLoading(true);
+    try {
+      const completeSignIn = await signIn.create({
+        identifier: emailAddress,
+        password,
+      });
+
+      await setActiveSI({ session: completeSignIn.createdSessionId });
+    } catch (error: any) {
+      alert(error.error[0].message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      {intendToRegister ? <Register /> : <Login />}
+    </View>
+  );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+  },
+  title: {
+    fontSize: 36,
+    fontWeight: "bold",
+    color: COLORS.cerulean,
+    textAlign: "center",
+    marginBottom: 20,
+  },
+  links: {
+    textAlign: "center",
+    paddingBottom: 2,
+  },
+});
 
 export default Susi;
